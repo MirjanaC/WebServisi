be747cdd7ed7ff99203b6d4adb1a75762af67b62
Zoran Luledžija <zoran.luledzija@outlook.com>
2016-06-29 12:47:19 +0200

    Updated insert script

--- scripts/insertTest.sql
+++ scripts/insertTest.sql
@@ -1,36 +1,54 @@
-INSERT INTO Users(user_name, user_lastname, user_email, user_password, user_role, project_id) 
-  VALUES('Admin','Adminic','admin@admin.com','admin','admin', 1),
-  ('Zoran','Luledzija','luledzija@gmail.com','zoki','programmer', 2),
-  ('Mirjana','Curcin','curcin@gmail.com','mima','programmer', 3),
-  ('Jovana','Ikonic','ikonic@gmail.com','jovana','programmer', 4),
-  ('Milos','Mitrovic','mitrovic@gmail.com','milos','programmer', 5),
-  ('Petar','Simeunovic','simeunovic@gmail.com','sima','programmer', 6),
-  ('Ilija','Svetic','svetic@gmail.com','ilija','programmer', 7),
-  ('Tamara','Simic','simic@gmail.com','tasa','programmer', 8);
+INSERT INTO Users(user_name, user_lastname, user_email, user_password, user_role) 
+  VALUES('Admin','Adminic','admin@admin.com','admin','admin'),
+  ('Zoran','Luledzija','luledzija@gmail.com','zoki','programmer'),
+  ('Mirjana','Curcin','curcin@gmail.com','mima','programmer'),
+  ('Jovana','Ikonic','ikonic@gmail.com','jovana','programmer'),
+  ('Milos','Mitrovic','mitrovic@gmail.com','milos','programmer'),
+  ('Petar','Simeunovic','simeunovic@gmail.com','sima','programmer'),
+  ('Ilija','Svetic','svetic@gmail.com','ilija','programmer'),
+  ('Tamara','Simic','simic@gmail.com','tasa','programmer');
   
-INSERT INTO Teams(team_name, user_id)
-  VALUES('Team1', 2),
-  ('Team1', 3),
-  ('Team2', 4),
-  ('Team2', 5),
-  ('Team2', 6),
-  ('Team3', 7),
-  ('Team3', 8);
+INSERT INTO Teams(team_name)
+  VALUES('Team1'),
+        ('Team2'),
+        ('Team3'),
+        ('Team4'),
+        ('Team5'),
+        ('Team6');
+        
+INSERT INTO TeamUsers(team_id, user_id)
+  VALUES(1,1),
+        (2,2),
+        (3,3),
+        (4,4),
+        (5,5),
+        (6,6)
+        (6,3);
   
-INSERT INTO Projects(project_name, user_id, task_id) 
-  VALUES('Project1', 1, 2),
-        ('Project2', 2, 1),
-        ('Project3', 3, 3),
-        ('Project4', 4, 4),
-        ('Project5', 5, 5),
-        ('Project6', 6, 6),
-        ('Project7', 7, 7),
-        ('Project8', 8, 8),
-        ('Project9', 9, 5),
-        ('Project6', 5, 2);
+INSERT INTO Projects(project_name, project_code, team_id) 
+  VALUES('Project1', 'PR-1', 1),
+        ('Project2', 'PR-2', 2),
+        ('Project3', 'PR-3', 3),
+        ('Project4', 'PR-4', 4),
+        ('Project5', 'PR-5', 5),
+        ('Project6', 'PR-6', 6);
   
-INSERT INTO Tasks(task_mark, task_title, task_creationDate, task_userCreator, task_userAssigned, task_description, task_priority, task_status, user_id, project_id)
-  VALUES('Mark1', 'Title 1', '2010-01-11', 'Covek1', 'Covek2', 'Opis', 'Trivial', 'To Do', 1, 1);
+INSERT INTO Tasks(task_mark, task_title, task_creationDate, task_description, task_priority, task_status, project_id)
+  VALUES('PR-1-1', 'Title 1', '2010-01-11', 'Opis', 'Trivial', 'To Do', 1),
+        ('PR-1-2', 'Title 2', '2010-01-11', 'Opis', 'Trivial', 'To Do', 2),
+        ('PR-1-3', 'Title 3', '2010-01-11', 'Opis', 'Trivial', 'To Do', 3),
+        ('PR-1-4', 'Title 4', '2010-01-11', 'Opis', 'Trivial', 'To Do', 4),
+        ('PR-1-5', 'Title 5', '2010-01-11', 'Opis', 'Trivial', 'To Do', 5),
+        ('PR-1-6', 'Title 6', '2010-01-11', 'Opis', 'Trivial', 'To Do', 6);
+        
+ INSERT INTO TaskUsers(task_id, task_creator, user_id)
+  VALUES(1, 'C', 1),
+        (2, 'C', 2),
+        (3, 'C', 3),
+        (4, 'C', 4),
+        (5, 'C', 5),
+        (6, 'C', 6),
+        (6, 'D', 4);
   
 INSERT INTO Comments(comment_creationDate, comment_text, comment_userCreator, user_id, task_id)
   VALUES('2016-06-25', 'Komentar', 'Creator', 1, 1);
